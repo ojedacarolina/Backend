a668dcd0308e21de47c06e97202a00a73a174302
ojedacarolina <115271444+ojedacarolina@users.noreply.github.com>
2023-06-09 21:48:07 -0300

    añadi var cors

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const config = require("config.json");
-
+var cors = require('cors');
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
